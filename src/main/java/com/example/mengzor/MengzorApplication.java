55226599048fa912737aad9a374769bcf837b18e
gianlucanotaro <gianlucanotaro@protonmail.com>
2024-02-28 14:05:24 +0100

    initian commit

---
+++ src/main/java/com/example/mengzor/MengzorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mengzor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MengzorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MengzorApplication.class, args);
+	}
+
+}
