e449c249f4d4ca0c732f2cc581f713ea3debff36
gianlucanotaro <gianlucanotaro@protonmail.com>
2024-03-09 23:25:16 +0100

    add test api endpoint

--- src/main/java/com/example/mengzor/controller/ExerciseController.java
+++ src/main/java/com/example/mengzor/controller/ExerciseController.java
@@ -1,4 +1,27 @@
 package com.example.mengzor.controller;
 
+import com.example.mengzor.model.Exercise;
+import com.example.mengzor.service.ExerciseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exercises") // Base path for all endpoints in this controller
 public class ExerciseController {
+
+    private final ExerciseService exerciseService;
+
+    @Autowired
+    public ExerciseController(ExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+    }
+
+    @GetMapping
+    public List<Exercise> getAllExercises() {
+        return exerciseService.getAllExercises();
+    }
 }
--- src/main/java/com/example/mengzor/service/ExerciseService.java
+++ src/main/java/com/example/mengzor/service/ExerciseService.java
@@ -1,4 +1,22 @@
 package com.example.mengzor.service;
 
+import com.example.mengzor.model.Exercise;
+import com.example.mengzor.repository.ExerciseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ExerciseService {
+    private final ExerciseRepository exerciseRepository;
+
+    @Autowired
+    public ExerciseService(ExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    public List<Exercise> getAllExercises() {
+        return exerciseRepository.findAll();
+    }
 }
